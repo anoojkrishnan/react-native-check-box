d87d9c07b29395ca71b7b6cd105a2263e8ffc117
Anooj Krishnan G <anooj.krishnan@synclovis.com>
2024-04-23 11:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ import {
     View,
     Image,
     Text,
-    TouchableHighlight,
-    ViewPropTypes as RNViewPropTypes,
+    TouchableHighlight
 } from 'react-native';
 import PropTypes from 'prop-types';
-
+import {ViewPropTypes as RNViewPropTypes} from 'deprecated-react-native-prop-types';
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
 
 export default class CheckBox extends Component {
